aad3edd43260a5a49090231a131cd83f4731a1a0
Gunnar R <gunnarrosenberg@gmail.com>
2023-01-25 13:17:20 -0800

    fix dep

--- mix.exs
+++ mix.exs
@@ -47,7 +47,7 @@ defmodule Openai.MixProject do
     [
       {:json, "~> 0.3.0"},
       {:httpoison, "~> 1.8"},
-      {:mock, "~> 0.3.6"},
+      {:mock, "~> 0.3.6", only: :test},
       {:mix_test_watch, "~> 1.0"},
       {:ex_doc, ">= 0.19.2", only: :dev}
     ]
